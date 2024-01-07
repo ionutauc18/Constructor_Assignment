c4106eb16d85397ef341d1965ce1033194254385
ionut <ionutcatana1805@gmail.com>
2024-01-07 23:31:43 +0000

    Add project files.

---
+++ Chain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    public class Chain
+    {
+        // Creating a constructor with only one parameter, an integer, is provided
+        public Chain(int age1) : this(age1, "Arsenal FC")
+        {
+            Console.WriteLine(age1 + " and your favourite football team is " + FavouriteTeam);
+        }
+        // Creating a constructor with only one parameter, a string, is provided
+        public Chain(string favouriteTeam1) : this(31, favouriteTeam1)
+        {
+            Console.WriteLine(favouriteTeam1 + " and your age is " + Age);
+        }
+        // Creating a constructor with both parametera, a string and an integer, are provided
+        public Chain(int age, string favouriteTeam)
+        {
+            Age = age;
+            FavouriteTeam = favouriteTeam;
+        }
+        int Age { get; set; }
+        string FavouriteTeam { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a const variable
+            const string const1 = "Your favourite football team is: ";
+            const string const2 = "Your age is: ";
+            Console.WriteLine("Enter your favourite football team or your age: ");
+            // Creating a variable using the var keyword
+            var answer = Console.ReadLine();
+            if (int.TryParse(answer, out int x))
+            {
+                Console.WriteLine(const2);
+                Chain newChain = new Chain(x);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(const1);
+                Chain newChain = new Chain(answer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
